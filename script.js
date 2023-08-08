5eb7c17dbdfe973eda09409c73d981db7aa94b93
Yasar <tneniitoo@gmail.com>
2023-08-08 14:31:28 -0300

    Update time

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ function showPrecios(){
     getPrecio().then((data) => {
         console.log(data);
         var fecha = new Date(data.last_update);
+        console.log(fecha);
         document.getElementById("datos").innerHTML +=`
         <h2>Dólar Azul Guiño Guiño 😉</h2>
         <h3>Venta: $${data.blue.value_sell} <br>
         Compra: $${data.blue.value_buy} <br>
         Promedio: $${data.blue.value_avg} <br>
         </h3>
-        <h5>Ultima actualización:<br>
-        ${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}<br>
-        ${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}<br>
+        <h5>Ultima actualización:
+        <br>
+        ${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}      ${fecha.getHours()}:${fecha.getMinutes()}<br>
         </h5>
         `;
     });
